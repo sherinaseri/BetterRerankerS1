740197b7a4f8a82fea776b8ce072c030628bdd30
sherinaseri <shahrzad.na3ri@gmail.com>
2022-10-27 22:33:52 +0000

    change the input directories

--- run_module.sh
+++ run_module.sh
@@ -5,10 +5,10 @@ python3 rerank.py \
         --mode "AUTO" \
         --seed 42 \
         --model_name xlm-roberta-base \
-        --output_dir $BASE_PATH/better_P3/experiments/BetterRerankerS1/runs \
-        --checkpoint $BASE_PATH/multi_lang_dpr/checkpoints/msmarco/dpr_baseline_msmaroco_xlmr_multi_lang/ar_fa_ru/checkpoint_at_0.pth \
-        --collection_file $BASE_PATH/better_P3/data/BETTER_PHASE3_COMBO_ARABIC_FARSI_RUSSIAN/combined-corpus.jl \
-        --task_file $BASE_PATH/better_P3/data/BETTER_PHASE3_COMBO_ARABIC_FARSI_RUSSIAN/62e171cedebb2f0d2619f2f6.analytic_tasks.json \
+        --output_dir $BASE_PATH/better_P3/experiments/BetterRerankerS1/runs-test \
+        --checkpoint ../weights/checkpoint_at_0.pth \
+        --collection_file $BASE_PATH/better_P3/data/BETTER_PHASE3_COMBO_ARABIC_FARSI_RUSSIAN/REPRODUCE-GDPR-S1-AUTO/three-language-corpus.jl \
+        --task_file $BASE_PATH/better_P3/data/BETTER_PHASE3_COMBO_ARABIC_FARSI_RUSSIAN/REPRODUCE-GDPR-S1-AUTO/62e171cedebb2f0d2619f2f6.analytic_tasks.json \
         --test_qrels $BASE_PATH/better_P3/data/BETTER_PHASE3_COMBO_ARABIC_FARSI_RUSSIAN/IR-relevance-assessments.qrels.GALAGO \
-        --test_run $BASE_PATH/better_P3/experiments/BetterRerankerS1/runs/62e171cedebb2f0d2619f2f6.FINAL.out \
-        --rerank_topK 1000
+        --test_run $BASE_PATH/better_P3/data/BETTER_PHASE3_COMBO_ARABIC_FARSI_RUSSIAN/REPRODUCE-GDPR-S1-AUTO/62e171cedebb2f0d2619f2f6.FINAL.out \
+        --rerank_topK 300
